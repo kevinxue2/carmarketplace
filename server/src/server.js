efe242684da17a83a0785a8c8eaadd25ddf74cb3
kevinxue2 <kevin.xue27@gmail.com>
2025-09-03 13:10:36 -0400

    disable cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,36 +1,36 @@
 import express from "express";
 
-import cors from 'cors'
+// import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://carmarketplace-d54d.vercel.app'
-];
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'https://carmarketplace-d54d.vercel.app'
+// ];
 
-app.use(cors({
-  origin: (origin, cb) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      cb(null, true);
-    } else {
-      cb(new Error('CORS not allowed for ' + origin));
-    }
-  },
-  credentials: true
-}));
+// app.use(cors({
+//   origin: (origin, cb) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error('CORS not allowed for ' + origin));
+//     }
+//   },
+//   credentials: true
+// }));
 
 app.get("/", (_, response) =>
   response.json({ info: "Express app with Supabase" })
 );
 
-// app.listen(3001, () =>
-//   console.log(
-//     new Date().toLocaleTimeString() + `: Server is running on port ${3001}...`
-//   )
-// );
+app.listen(3001, () =>
+  console.log(
+    new Date().toLocaleTimeString() + `: Server is running on port ${3001}...`
+  )
+);
 
 import {authRoutes} from './routes/auth.routes.js';
 // const userRoutes = require('./routes/user.routes');
